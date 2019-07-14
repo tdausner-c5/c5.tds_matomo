7655282530f4eff1dd95ef2982740eeefa30977d
tdausner <thomas@dausner.de>
2019-07-14 18:57:51 +0200

    README.md added

---
+++ README.md
@@ -0,0 +1,12 @@
+# tds_matomo
+Concrete5 MATOMO plugin
+
+Add the MATOMO add-on block to your web sites **Stacks & Global Areas** > **Global Areas** to one of the defined 
+global areas. I'd recommend a global area at the web page bottom, or - as it's best to remember - the 
+first or the last of the global areas. I prefer the last global area.
+
+The add-on needs holds two entry fields:
+* the MATOMO system's URL without leading http:// or https://
+* site ID from the target MATOMO system
+
+Adding the MATOMO add-on block more than once is not helpful, but it wouldn't mess up your pages.
--- blocks/tds_matomo/view.php
+++ blocks/tds_matomo/view.php
@@ -4,23 +4,31 @@ if (Page::getCurrentPage()->isEditMode())
 {
     echo '<pre>'.t('Place holder for Matomo embedding (contains no visible content)').'</pre>';
 }
+else
+{
+    ?>
 
+    <script type="text/javascript">
+        if ( typeof ( _paq ) === "undefined" )
+        {
+            var _paq = _paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push( ['trackPageView'] );
+            _paq.push( ['enableLinkTracking'] );
+            ( function () {
+                var u = "//<?php echo $url ?>/";
+                _paq.push( ['setTrackerUrl', u + 'piwik.php'] );
+                _paq.push( ['setSiteId', '<?php echo $websiteId ?>'] );
+                var d = document, g = d.createElement( 'script' ), s = d.getElementsByTagName( 'script' )[0];
+                g.type = 'text/javascript';
+                g.async = true;
+                g.defer = true;
+                g.src = u + 'piwik.js';
+                s.parentNode.insertBefore( g, s );
+            } )();
+        }
+    </script>
+
+    <?php
+}
 ?>
-
-<script type="text/javascript">
-    var _paq = _paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push( ['trackPageView'] );
-    _paq.push( ['enableLinkTracking'] );
-    ( function () {
-        var u = "//<?php echo $url ?>/";
-        _paq.push( ['setTrackerUrl', u + 'piwik.php'] );
-        _paq.push( ['setSiteId', '<?php echo $websiteId ?>'] );
-        var d = document, g = d.createElement( 'script' ), s = d.getElementsByTagName( 'script' )[0];
-        g.type = 'text/javascript';
-        g.async = true;
-        g.defer = true;
-        g.src = u + 'piwik.js';
-        s.parentNode.insertBefore( g, s );
-    } )();
-</script>
