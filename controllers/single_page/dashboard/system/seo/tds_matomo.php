5f586d835a83d8029589b8bbe81a6ff0098454bc
tdausner <thomas@dausner.de>
2020-02-28 16:27:35 +0100

    PRB 0.9.2 fixes

--- controllers/single_page/dashboard/system/seo/tds_matomo.php
+++ controllers/single_page/dashboard/system/seo/tds_matomo.php
@@ -6,27 +6,34 @@
  */
 namespace Concrete\Package\TdsMatomo\Controller\SinglePage\Dashboard\System\Seo;
 
+use Concrete\Core\Config\Repository\Repository;
 use Concrete\Core\Cache\Page\PageCache;
-use Concrete\Core\Http\ResponseFactoryInterface;
 use Concrete\Core\Page\Controller\DashboardPageController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Concrete\Core\Http\Request;
 
 class TdsMatomo extends DashboardPageController  {
 
     public function view()
     {
+        /**
+         * @var $config Repository
+         */
         $config = $this->app->make('config/database');
         $this->set('tds_matomo_serverurl', $config->get('tds_matomo.serverurl'));
         $this->set('tds_matomo_siteid', $config->get('tds_matomo.siteid'));
-
         $request = $this->getRequest();
 
+        /**
+         * @var $errmsg string
+         */
+        $errmsg = '';
         if ($request->getMethod() == 'POST')
         {
-            $errmsg = '';
             if ($this->token->validate('update_tracking_code'))
             {
                 $matomoUrl = trim($request->get('tds_matomo_serverurl'));
-                $matomoUrl = $checkUrl = preg_replace("#(https?://)?([^/]+)#", "$2", $matomoUrl);
+                $matomoUrl = $checkUrl = preg_replace("#(https?://[^/]+)#", "$1", $matomoUrl);
                 if (!preg_match("/^[0-9a-z.-]+$/i", $matomoUrl))
                 {
                     if (function_exists('idn_to_ascii'))
@@ -46,24 +53,28 @@ To utilize an internationalized domain name (IDN) you must have <code>php_intl</
                     {
                         $errmsg = t('Matomo server URL must not be empty.');
                     }
-                    elseif (!preg_match('/^([a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?\.){1,2}[a-z0-9]{0,63}$/i', $checkUrl))
+                    elseif (!preg_match('/^https?:\/\/([a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?\.){1,2}[a-z0-9]{0,63}\/?$/i', $checkUrl))
                     {
-                        $errmsg = t('Matomo server URL must be a valid server URL.');
+                        $errmsg = t('Matomo server URL must be a valid URL (including protocol http:// or https://).');
                     }
                     elseif (empty($siteId))
                     {
                         $errmsg = t('Matomo site ID must by a number greater equal 1.');
-                    } else
+                    }
+                    else
                     {
                         $config->save('tds_matomo.serverurl', trim($matomoUrl));
-                        $config->save('tds_matomo.siteid', $request->get('tds_matomo_siteid'));
+                        $config->save('tds_matomo.siteid', $siteId);
 
                         $pageCache = PageCache::getLibrary();
                         if (is_object($pageCache))
                         {
                             $pageCache->flush();
                         }
-                        return $this->app->make(ResponseFactoryInterface::class)->redirect('/dashboard/system/seo/tds_matomo/saved', 302);
+
+                        $server = Request::getInstance()->server;
+                        $prefix = substr( DIR_BASE, strlen($server->get('DOCUMENT_ROOT')));
+                        return new RedirectResponse($prefix . '/index.php/dashboard/system/seo/tds_matomo/saved', 302);
                     }
                 }
             }
@@ -87,5 +98,25 @@ To utilize an internationalized domain name (IDN) you must have <code>php_intl</
         $this->view();
     }
 
+    public function removed()
+    {
+        /**
+         * @var $config Repository
+         */
+        $config = $this->app->make('config/database');
+        $config->save('tds_matomo.serverurl', '');
+        $config->save('tds_matomo.siteid', '');
+
+        $pageCache = PageCache::getLibrary();
+        if (is_object($pageCache))
+        {
+            $pageCache->flush();
+        }
+        $this->set('message', implode(PHP_EOL, [
+            t('Matomo webtracking data removed.'),
+            t('Cached files removed.')
+        ]));
+        $this->view();
+    }
 }
 
--- single_pages/dashboard/system/seo/tds_matomo/view.php
+++ single_pages/dashboard/system/seo/tds_matomo/view.php
@@ -1,13 +1,24 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.');
 
-$app = \Concrete\Core\Support\Facade\Facade::getFacadeApplication();
+/**
+ * TDS Matomo embedding
+ *
+ * Copyright 2020 - TDSystem Beratung & Training - Thomas Dausner
+ */
 
-/** @var Concrete\Core\Form\Service\Form $form */
+use Concrete\Core\Support\Facade\Facade;
+
+$app = Facade::getFacadeApplication();
+
+/**
+ * @var $form Concrete\Core\Form\Service\Form
+ * @var $tds_matomo_serverurl string
+ * @var $tds_matomo_siteid integer
+ */
 $form = $app->make('helper/form');
 ?>
 
-<p class="help-block"><?php echo t("Specify Matomo server URL and site ID supplied by your Matomo web tracking provider.
-Server URL protocol (http|hhtps) can be ommitted as it is replaced by the web site's protocol."); ?></p>
+<p class="help-block"><?php echo t("Specify Matomo server URL (including http:// or https:// protocol) and site ID supplied by your Matomo web tracking provider."); ?></p>
 
 <form id="tracking-code-form" action="<?php echo $view->action(''); ?>" method="post">
     <?php echo $app->make('helper/validation/token')->output('update_tracking_code'); ?>
@@ -19,13 +30,31 @@ Server URL protocol (http|hhtps) can be ommitted as it is replaced by the web si
         </p>
         <p>
             <?php echo $form->label('tds_matomo_siteid', t('Site ID')); ?>
-            <?php echo $form->number('tds_matomo_siteid', $tds_matomo_siteid, [ "min" => "1" ]); ?>
+            <?php echo $form->number('tds_matomo_siteid', $tds_matomo_siteid, ["min" => "1"]); ?>
         </p>
     </div>
 
     <div class="ccm-dashboard-form-actions-wrapper">
         <div class="ccm-dashboard-form-actions">
-            <button type="submit" class="btn btn-primary pull-right" name="tracking-code-form"><?php echo t('Save'); ?></button>
+            <button type="button" class="btn btn-default pull-left"
+                    name="tracking-code-form-remove"><?php echo t('Remove'); ?></button>
+            <button type="submit" class="btn btn-primary pull-right"
+                    name="tracking-code-form"><?php echo t('Save'); ?></button>
         </div>
     </div>
 </form>
+
+<script type="text/javascript">
+    ( function ( $ ) {
+        $( document ).ready( function () {
+            $( '.ccm-dashboard-form-actions button[type=button]' ).click( function ( e ) {
+
+                e.preventDefault();
+                ConcreteAlert.confirm( '<p><?php echo t('Remove Matomo tracking data?'); ?></p>', function () {
+                    location.href = location.href.replace(/\/(saved|removed)$/, '') + '/removed';
+                }, 'btn-danger', '<?php echo t('Remove'); ?>' );
+
+            } );
+        } );
+    } )( window.jQuery );
+</script>
